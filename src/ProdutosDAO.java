6e192e82bc2426cfe859f5997db60a426706ce5e
YasmineCruz92 <yasmineramalhocruz@gmail.com>
2023-08-08 11:40:45 -0300

    botao cadastrar

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
@@ -10,8 +6,8 @@
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,11 +18,28 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public boolean cadastrarProduto (ProdutosDTO produto){
+        
+        try {
+        
+            conn = new conectaDAO().connectDB();
+
+            String query = 
+                "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
         
         
-        //conn = new conectaDAO().connectDB();
-        
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString(1, produto.getNome());
+            preparedStmt.setInt(2,produto.getValor());
+            preparedStmt.setString(3, produto.getStatus());
+            
+            preparedStmt.executeUpdate();
+            return true;
+            
+        } catch (SQLException e) {
+            System.out.println("Deu erro " + e.getMessage());
+            return false;
+        }
         
     }
     
--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -140,16 +143,25 @@ public class cadastroVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_cadastroNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
-        ProdutosDTO produto = new ProdutosDTO();
+     ProdutosDTO produto = new ProdutosDTO();
         String nome = cadastroNome.getText();
         String valor = cadastroValor.getText();
         String status = "A Venda";
         produto.setNome(nome);
-        produto.setValor(Integer.parseInt(valor));
         produto.setStatus(status);
         
+        try {
+            produto.setValor(Integer.parseInt(valor));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Deu erro, verifique o valor informado");
+        }
+        
         ProdutosDAO produtodao = new ProdutosDAO();
-        produtodao.cadastrarProduto(produto);
+        if (produtodao.cadastrarProduto(produto)) {
+            JOptionPane.showMessageDialog(null, "Foi cadastrado com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Deu erro, verifique se os dados estão corretos!");
+        }
         
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
